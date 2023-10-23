0188c811375ae2d4e7c248a1a49854f0fe6984c7
Ayush Sharma <ayush2162020@gmail.com>
2023-10-24 02:48:20 +0530

    fix(ci): bump local mongodb to cloud instance

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
+import { MONGO_URI } from "../utils/env";
 
 const options = { useNewUrlParser: true, useUnifiedTopology: true } as mongoose.ConnectOptions;
 
-export const ssoDb = mongoose.createConnection(`mongodb://127.0.0.1:27017/sso`, options);
+export const ssoDb = mongoose.createConnection(`${MONGO_URI}`, options);
 
 ssoDb.once("open", () => {
     console.log("Database connection established!");
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,9 +1,12 @@
 import "dotenv/config";
 
-const JWT_SECRET_KEY = process.env?.JWT_SECRET_KEY ?? "";
+const JWT_SECRET_KEY = process.env?.JWT_SECRET_KEY ?? "",
+    MONGO_URI = process.env?.MONGO_URI ?? "";
 
 if (!JWT_SECRET_KEY) {
     throw new Error(`JWT_SECRET_KEY missing!`);
+} else if (!MONGO_URI) {
+    throw new Error(`MONGO_URI missing!`);
 }
 
-export { JWT_SECRET_KEY };
+export { JWT_SECRET_KEY, MONGO_URI };
